be550dfd435ca9f12520b7e4f216942bf7022172
Danya <117893294+hekkiii@users.noreply.github.com>
2023-10-27 04:26:17 +0300

    Just a template

---
+++ src/main/java/com/zaglushka/zaglushkaSOAP/ZaglushkaSoapApplication.java
@@ -0,0 +1,13 @@
+package com.zaglushka.zaglushkaSOAP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaglushkaSoapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaglushkaSoapApplication.class, args);
+	}
+
+}
